42b92d91ec4bc6eb6e830093e1fc395e7efb9cab
Yuda Cogati <yudapc@gmail.com>
2019-09-05 12:32:43 +0700

    added filter pattern

---
+++ src/filter_pattern/OrCriteria.java
@@ -0,0 +1,27 @@
+package filter_pattern;
+
+import java.util.List;
+
+public class OrCriteria implements Criteria {
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public OrCriteria(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        List<Person> firstCriteria = criteria.meetCriteria(persons);
+        List<Person> otherCriteriaItems = otherCriteria.meetCriteria(persons);
+
+        for (Person person : otherCriteriaItems) {
+            if (!firstCriteria.contains(person)) {
+                firstCriteria.add(person);
+            }
+        }
+
+        return firstCriteria;
+    }
+}

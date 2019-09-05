3efa983679bf4a1d718f87d6b0996115f746276a
Yuda Cogati <yudapc@gmail.com>
2019-09-05 11:03:11 +0700

    added factory pattern

---
+++ src/factory_pattern/FactoryPatternDemo.java
@@ -0,0 +1,15 @@
+package factory_pattern;
+
+public class FactoryPatternDemo {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape circle = shapeFactory.getShape("circle");
+        circle.draw();
+
+        Shape rectangle = shapeFactory.getShape("rectangle");
+        rectangle.draw();
+
+        Shape square = shapeFactory.getShape("square");
+        square.draw();
+    }
+}
